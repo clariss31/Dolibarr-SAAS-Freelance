322ccce9054be3c137fa416a902ba7064c75f31f
clarisse31 <clarrise.ferand@gmail.com>
2026-03-27 11:52:57 +0100

    Correction des couleurs

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,8 +25,8 @@ export default function RootLayout({
 }>) {
   return (
     <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      lang="fr"
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased dark`}
     >
       <body className="flex min-h-full flex-col">{children}</body>
     </html>
